eaeff156760e8771f8384a049fce505103f9fd43
Robson Kerner <robson@arkaweb.com.br>
2011-10-24 20:23:24 -0200

    estrutura

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta content="text/html; charset=UTF-8" http-equiv="content-type" />
+        <title>Certificação ZCE</title>
+        <link type="text/css" rel="stylesheet" href="css/template.css" />
+    </head>
+    <body>
+        <div id="top-bar">
+            <a href="#" title="ZCE">PHPET - Certificação ZCE</a>
+        </div>
+        <div id="container">
+            
+        </div>
+    </body>
+</html>
